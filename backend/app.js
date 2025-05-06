58661af81c19e92b6ddd2e1f755d5d29497ebd9d
rudra11911 <rudraprataprp9651567619@gmail.com>
2025-05-07 03:19:09 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,6 @@ const PORT = process.env.PORT || 5000;
 
 // Create an express application
 const app = express();
-const cors = require("cors");
 
 app.use(cors({
   origin: 'https://workxplore.vercel.app',
